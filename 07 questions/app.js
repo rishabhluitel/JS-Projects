44b7d581680fba871f8ef573029022a931c6e2da
Rishabh Luitel <47209302+rishabhluitel@users.noreply.github.com>
2025-03-22 06:58:20 +0000

    questions final

--- 07 questions/app.js
+++ 07 questions/app.js
@@ -1,4 +1,26 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach(question => {
+    let btn=question.querySelector('.question-btn');
+    btn.addEventListener('click',()=>{
+        questions.forEach(element => {
+            console.log(element)
+            if (element!==question){
+                element.classList.remove('show-text');
+            }
+        }); 
+            question.classList.toggle('show-text');
+    })
+});
+
+
 // traversing the dom
 
+// let btns=document.querySelectorAll('.question-btn');
 
+// btns.forEach((btn) => {
+//      btn.addEventListener('click',(e)=>{
+//         let changeBtn=e.currentTarget.parentElement.parentElement;
+//         changeBtn.classList.toggle('show-text');
+//     })
+// });
